0d6a234aa3adf36856fe43cb7d9ce50daca0c15f
RikD <rik@instorebroadcast,com>
2018-07-06 10:19:13 +0200

    Initial commit

---
+++ SettingsActivityTest/SettingsActivityTest.Android/AndroidAppService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Plugin.CurrentActivity;
+using SettingsActivityTest.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AndroidAppService))]
+namespace SettingsActivityTest.Droid
+{
+    public class AndroidAppService : IAppService
+    {
+        Context CurrentContext => CrossCurrentActivity.Current.Activity;
+
+        public Task OpenWifiSettings()
+        {
+            Intent intent = new Intent("android.intent.action.MAIN");
+            intent.SetComponent(ComponentName.UnflattenFromString("com.android.settings/.wifi.WifiSettings"));
+            intent.AddCategory("android.intent.category.DEFAULT");
+
+            return Task.Run(() =>
+            {
+                try
+                {
+                    CurrentContext.StartActivity(intent);
+                }
+                catch (Exception)
+                {
+                    Toast.MakeText(CurrentContext.ApplicationContext, "Unable to open Wifi Settings", ToastLength.Short);
+                }
+            });
+
+        }
+    }
+}
---
+++ SettingsActivityTest/SettingsActivityTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SettingsActivityTest
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            btnOpenWifiSettings.Clicked += BtnOpenWifiSettings_Clicked;
+		}
+
+        private async void BtnOpenWifiSettings_Clicked(object sender, EventArgs e)
+        {
+            IAppService appService = DependencyService.Get<IAppService>();
+            await appService.OpenWifiSettings();
+        }
+    }
+}
